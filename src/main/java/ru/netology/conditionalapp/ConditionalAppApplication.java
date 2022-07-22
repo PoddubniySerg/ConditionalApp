bd46161ea193e5cf94e4d7d39f409f7286029693
Poddubniy Sergey <poddubniysergey1981@gmail.com>
2022-07-23 00:33:28 +0300

    import optimization

--- src/main/java/ru/netology/conditionalapp/ConditionalAppApplication.java
+++ src/main/java/ru/netology/conditionalapp/ConditionalAppApplication.java
@@ -2,8 +2,6 @@ package ru.netology.conditionalapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConditionalAppApplication {
